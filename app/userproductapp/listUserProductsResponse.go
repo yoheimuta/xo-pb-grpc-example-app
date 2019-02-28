9c66018e49b1c2f8ccb469f400efaf7b14748eea
yoheimuta <yoheimuta@gmail.com>
2019-02-28 15:53:46 +0900

    feat: Added userproductpb

--- app/userproductapp/listUserProductsResponse.go
+++ app/userproductapp/listUserProductsResponse.go
@@ -1,8 +1,26 @@
 package userproductapp
 
-import "github.com/yoheimuta/xo-pb-example-app/infra/expmysql/expmodels"
+import (
+	"github.com/yoheimuta/xo-pb-example-app/infra/expgenproto/userproductpb"
+	"github.com/yoheimuta/xo-pb-example-app/infra/expmysql/expmodels"
+)
 
 // ListUserProductsResponse represents a response for ListUserProducts method.
 type ListUserProductsResponse struct {
 	UserProducts []*expmodels.UserProduct
+}
+
+// ToListUserProductResponsePB converts to a ListUserProductResponse pb.
+func (r *ListUserProductsResponse) ToListUserProductResponsePB() *userproductpb.ListUserProductResponse {
+	var products []*userproductpb.ListUserProductResponse_Product
+	for _, p := range r.UserProducts {
+		products = append(products, &userproductpb.ListUserProductResponse_Product{
+			UserProductId: p.UserProductID,
+			Title:         p.Title,
+			Description:   p.Description,
+		})
+	}
+	return &userproductpb.ListUserProductResponse{
+		Products: products,
+	}
 }
